36c402ad7977c7bc2df720fa0d1d921e8ac21361
Peace Melodi <peacemelodi1@gmail.com>
2024-07-07 15:29:35 +0100

    new

--- src/components/pages/Project/Project.jsx
+++ src/components/pages/Project/Project.jsx
@@ -11,7 +11,7 @@ const Project = () => {
       <input type="text" placeholder='Search Projects'  className='bg-[#052A49] pl-[3.6rem] w-[30rem] h-[3rem] rounded-[1rem] IPad:w-[20rem] side-phone:w-[14rem]'/>
       <CiSearch  className='text-[#9E9EA2]  absolute  mr-[26rem] IPad:mr-[16rem] text-[1.5rem] side-phone:mr-[10.5rem]'/>
         </div>
-        <div className='flex justify-center items-center side-phone:pb-[2rem]'>
+        <div className='flex justify-center items-center side-phone:pb-[5rem]'>
  <button className='text-white bg-[#067EF6] px-4 py-3.5 text-[0.9rem] rounded-[2rem] IPad:text-[0.7rem] side-phone:text-[0.6rem] transition duration-300 ease-in-out transform hover:scale-105'>Add New Project</button>
     
         </div>
