4a24fecdeb551118a41323f696c1af562fefae35
Peace Melodi <peacemelodi1@gmail.com>
2024-07-12 02:16:29 +0100

    new

--- src/components/pages/Career/NewCareer.jsx
+++ src/components/pages/Career/NewCareer.jsx
@@ -18,32 +18,32 @@ const NewCareer = () => {
           <label htmlFor="" className='text-[grey] IPad:pr-[30rem] side-phone:pr-[11.9rem] pr-[42.5rem]'>Job Title</label>
           <input type="text"  className=' side-phone:w-[19rem] IPad:w-[40rem] w-[49rem] h-[3rem] rounded-[0.6rem]  placeholder-gray-500 bg-[#052A49] text-white pl-6' placeholder="Job Title"/>
         </div>
-        <div className='flex flex-col justify-center items-center pt-[4rem] gap-[1rem]'>
+        <div className='flex flex-col justify-center items-center pt-[3rem] gap-[1rem]'>
           <label htmlFor="" className='text-[grey] IPad:pr-[30rem] side-phone:pr-[11.9rem] pr-[42.5rem]'>Job Type</label>
           <input type="text"  className=' side-phone:w-[19rem] IPad:w-[40rem] w-[49rem] h-[3rem] rounded-[0.6rem]  placeholder-gray-500 bg-[#052A49] text-white pl-6' placeholder="Job Type"/>
         </div>
-        <div className='flex flex-col justify-center items-center pt-[4rem] gap-[1rem]'>
+        <div className='flex flex-col justify-center items-center pt-[3rem] gap-[1rem]'>
           <label htmlFor="" className='text-[grey] IPad:pr-[30rem] side-phone:pr-[11.9rem] pr-[42.5rem]'>Location</label>
           <input type="text"  className=' side-phone:w-[19rem] IPad:w-[40rem] w-[49rem] h-[3rem] rounded-[0.6rem]  placeholder-gray-500 bg-[#052A49] text-white pl-6' placeholder="Location"/>
         </div>
-        <div className='flex flex-col justify-center items-center pt-[4rem] gap-[1rem]'>
+        <div className='flex flex-col justify-center items-center pt-[3rem] gap-[1rem]'>
           <label htmlFor="" className='text-[grey] IPad:pr-[28rem] side-phone:pr-[10rem] pr-[40rem]'>Compensation</label>
           <input type="text"  className=' side-phone:w-[19rem] IPad:w-[40rem] w-[49rem] h-[3rem] rounded-[0.6rem]  placeholder-gray-500 bg-[#052A49] text-white pl-6' placeholder="Compensation"/>
         </div>
-        <div className='flex flex-col justify-center items-center pt-[4rem] gap-[1rem]'>
+        <div className='flex flex-col justify-center items-center pt-[3rem] gap-[1rem]'>
           <label htmlFor="" className='text-[grey] IPad:pr-[28rem] side-phone:pr-[10rem] pr-[40rem]'>Job Description</label>
           <input type="text"  className=' IPad:w-[40rem] side-phone:w-[20rem] w-[49rem] h-[10rem] rounded-[0.4rem] pb-[6rem]   placeholder-gray-500 bg-[#052A49] text-white pl-5' placeholder="Job Description"/>
               </div>
-              <div className='flex flex-col justify-center items-center pt-[4rem] gap-[1rem]'>
+              <div className='flex flex-col justify-center items-center pt-[3rem] gap-[1rem]'>
           <label htmlFor="" className='text-[grey] IPad:pr-[25rem] side-phone:pr-[8rem] pr-[38rem]'>Key Responsibilities</label>
           <input type="text"  className=' IPad:w-[40rem] side-phone:w-[20rem] w-[49rem] h-[10rem] rounded-[0.4rem] pb-[6rem]   placeholder-gray-500 bg-[#052A49] text-white pl-5' placeholder="Key Responsibilities"/>
               </div>
-              <div className='flex flex-col justify-center items-center pt-[4rem] gap-[1rem]'>
+              <div className='flex flex-col justify-center items-center pt-[3rem] gap-[1rem]'>
           <label htmlFor="" className='text-[grey] IPad:pr-[27rem] side-phone:pr-[10.5rem] pr-[40.5rem]'>Qualifications</label>
           <input type="text"  className=' IPad:w-[40rem] side-phone:w-[20rem] w-[49rem] h-[10rem] rounded-[0.4rem] pb-[6rem]   placeholder-gray-500 bg-[#052A49] text-white pl-5' placeholder="Qualifications"/>
         </div>
           </div>
-          <div  className=' IPad:pr-[30.8rem] side-phone:pr-[0rem] pr-[40rem]  flex justify-center pt-[4rem] '> 
+          <div  className=' IPad:pr-[30.8rem] side-phone:pr-[0rem] pr-[40rem]  flex justify-center pt-[3rem] '> 
       <button   className= 'text-white bg-[#067EF6] px-8 py-3.5 text-[0.9rem] rounded-[2rem] transition duration-300 ease-in-out transform hover:scale-105'>  Continue </button>
       </div>
     
